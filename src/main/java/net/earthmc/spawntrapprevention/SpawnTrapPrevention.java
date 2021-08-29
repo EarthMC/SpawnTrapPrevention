7375e0677d0ed1bb9603c6d68d76afd1acba64c6
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2021-08-29 13:36:12 +0200

    Add caching

--- src/main/java/net/earthmc/spawntrapprevention/SpawnTrapPrevention.java
+++ src/main/java/net/earthmc/spawntrapprevention/SpawnTrapPrevention.java
@@ -1,6 +1,8 @@
 package net.earthmc.spawntrapprevention;
 
 import com.gmail.goosius.siegewar.utils.SiegeWarDistanceUtil;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.palmergames.bukkit.towny.TownySettings;
 import com.palmergames.bukkit.towny.event.damage.TownBlockPVPTestEvent;
 import com.palmergames.bukkit.towny.exceptions.TownyException;
@@ -16,8 +18,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 public class SpawnTrapPrevention extends JavaPlugin implements Listener {
+
+    private static final Cache<WorldCoord, Boolean> siegeZoneCache = CacheBuilder.newBuilder().expireAfterWrite(1, TimeUnit.MINUTES).build();
 
     @Override
     public void onEnable() {
@@ -26,13 +32,13 @@ public class SpawnTrapPrevention extends JavaPlugin implements Listener {
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
     public void onTownBlockTestPVP(TownBlockPVPTestEvent event) {
-        if (isCloseToNationSpawn(event.getTownBlock().getWorldCoord()) && !SiegeWarDistanceUtil.isLocationInActiveSiegeZone(toLocation(event.getTownBlock().getWorldCoord())))
+        if (isCloseToNationSpawn(event.getTownBlock().getWorldCoord()) && notInSiegeZone(event.getTownBlock().getWorldCoord()))
             event.setPvp(false);
     }
 
     @EventHandler(priority = EventPriority.NORMAL)
     public void onPlayerDeath(PlayerDeathEvent event) {
-        if (isCloseToNationSpawn(event.getEntity().getLocation()) && !SiegeWarDistanceUtil.isLocationInActiveSiegeZone(event.getEntity().getLocation())) {
+        if (isCloseToNationSpawn(event.getEntity().getLocation()) && notInSiegeZone(WorldCoord.parseWorldCoord(event.getEntity()))) {
             event.setKeepInventory(true);
             event.setKeepLevel(true);
             event.getDrops().clear();
@@ -74,5 +80,14 @@ public class SpawnTrapPrevention extends JavaPlugin implements Listener {
     private static Location toLocation(WorldCoord coord) {
         int tbSize = TownySettings.getTownBlockSize();
         return new Location(coord.getBukkitWorld(), coord.getX() * tbSize, 64, coord.getZ() * tbSize);
+    }
+
+    private boolean notInSiegeZone(WorldCoord worldCoord) {
+        try {
+            return siegeZoneCache.get(worldCoord, () -> !SiegeWarDistanceUtil.isLocationInActiveSiegeZone(toLocation(worldCoord)));
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            return !SiegeWarDistanceUtil.isLocationInActiveSiegeZone(toLocation(worldCoord));
+        }
     }
 }
